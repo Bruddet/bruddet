483a943f5d478084b0cda90b52f961fc01084643
Erling Van De Weijer <erling.weijer@bekk.no>
2024-08-27 15:41:40 +0200

    Style image and gif next to program list.

--- app/routes/($lang).program._index.tsx
+++ app/routes/($lang).program._index.tsx
@@ -83,42 +83,55 @@ export default function Program() {
   const params = useParams();
   return (
     <div className="flex flex-col grow items-center text-white font-serif">
-      <h1 className="text-5xl font-bold mb-12">{data?.title}</h1>
-      <div className="flex flex-col items-center text-center font-normal gap-4 text-xl py-12 px-0">
+      <h1 className="text-5xl font-bold mb-12 hidden">{data?.title}</h1>
+      <div className="flex md:flex-row flex-wrap w-[90vw] ">
+        <div className="w-full md:w-1/3 max-sm:hidden"></div>
+        <div className="w-full md:w-1/3 vertical-align: middle;">
+          {data?.links?.map((link, index) => (
+            <>
+              <Link
+                key={index}
+                to={
+                  link.slug?.current
+                    ? `${params.lang === "en" ? "/en/event/" : "/event/"}${
+                        link.slug.current
+                      }`
+                    : ""
+                }
+                onMouseEnter={() => {
+                  setGifUrl(
+                    urlFor(
+                      link.gif?.asset?._ref ?? link.image.asset?._ref ?? ""
+                    )
+                  );
+                }}
+                onMouseOut={() => {
+                  data?.gif && setGifUrl(urlFor(data.gif.asset?._ref ?? ""));
+                }}
+                className="z-10 align-middle w-full md:w-1/3 text-center"
+                aria-label={`${t(texts.labelText)} ${link.title}`}
+              >
+                <p className="p-4 hover:underline text-2xl lg:text-4xl">
+                  {link.title}
+                </p>
+                <div className="md:hidden w-full">
+                  <img
+                    className="inline-block"
+                    src={urlFor(link?.image?.asset?._ref ?? "")}
+                    alt={link?.image?.alt ?? ""}
+                  />
+                </div>
+              </Link>
+            </>
+          ))}
+        </div>
         {data?.gif && (
           <img
             src={gifUrl}
-            className="absolute w-2/3 right-[10vw] sm:w-1/3  lg:w-1/4 lg:right-[20vw] lg:bottom-5"
             alt={data.gif.alt}
+            className=" hidden md:block w-1/3 max-h-[500px] object-cover top-0 right-0"
           />
         )}
-
-        {data?.links?.map((link, index) => (
-          <Link
-            key={index}
-            to={
-              link.slug?.current
-                ? `${params.lang === "en" ? "/en/event/" : "/event/"}${
-                    link.slug.current
-                  }`
-                : ""
-            }
-            onMouseEnter={() => {
-              setGifUrl(
-                urlFor(link.gif?.asset?._ref ?? link.image.asset?._ref ?? "")
-              );
-            }}
-            onMouseOut={() => {
-              data?.gif && setGifUrl(urlFor(data.gif.asset?._ref ?? ""));
-            }}
-            className="z-10"
-            aria-label={`${t(texts.labelText)} ${link.title}`}
-          >
-            <p className="p-4 hover:underline text-2xl lg:text-4xl">
-              {link.title}
-            </p>
-          </Link>
-        ))}
       </div>
       <div className="mt-auto flex flex-col items-center text-lg lg:text-xl w-4/5 lg:w-2/3 z-10">
         <Newsletter />
