a28cdcb6330cb8446779594fa864dc148654c4df
Erling Van De Weijer <erling.weijer@bekk.no>
2024-09-04 10:21:35 +0200

    Add dice component.

--- cms/schemaTypes/objects/RichTextEditor.ts
+++ cms/schemaTypes/objects/RichTextEditor.ts
@@ -1,33 +1,33 @@
 export default {
-  name: 'content',
-  type: 'array',
-  title: 'Content',
+  name: "content",
+  type: "array",
+  title: "Content",
   of: [
     {
-      type: 'block',
+      type: "block",
       styles: [
-        {title: 'Normal', value: 'normal'},
-        {title: 'Heading 2', value: 'h2'},
-        {title: 'Heading 3', value: 'h3'},
-        {title: 'Heading 4', value: 'h4'},
-        {title: 'Heading 5', value: 'h5'},
-        {title: 'Heading 6', value: 'h6'},
+        { title: "Normal", value: "normal" },
+        { title: "Heading 2", value: "h2" },
+        { title: "Heading 3", value: "h3" },
+        { title: "Heading 4", value: "h4" },
+        { title: "Heading 5", value: "h5" },
+        { title: "Heading 6", value: "h6" },
       ],
     },
     {
-      type: 'customImage',
+      type: "customImage",
     },
     {
-      type: 'video',
+      type: "video",
     },
     {
-      type: 'quote',
+      type: "quote",
     },
     {
-      type: 'review',
+      type: "review",
     },
     {
-      type: 'expandableBlock',
+      type: "expandableBlock",
     },
   ],
-}
+};
