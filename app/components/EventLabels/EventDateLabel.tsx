09d682c697cb1b3fbf7859d1804cb8311538031c
Erling Van De Weijer <44138705+erlinvan@users.noreply.github.com>
2024-09-06 09:12:46 +0200

    Style image and gif next to program list. (#217)
    * Style image and gif next to program list.
    
    * Add information about event dates to the program page.

---
+++ app/components/EventLabels/EventDateLabel.tsx
@@ -0,0 +1,38 @@
+import { useTranslation } from "~/utils/i18n";
+import { formatDayAndDate } from "~/utils/dateAndTimeConverters";
+import { formatDateOnly, getDateLabel } from ".";
+
+type Props = {
+  dateObj: DateObject[];
+};
+
+type DateObject = {
+  date?: string | undefined;
+  url?: string | undefined;
+  _key?: string | undefined;
+};
+
+function EventDateLabel({ dateObj }: Props) {
+  const { language } = useTranslation();
+  const firstDate = dateObj[0].date ?? "";
+  const lastdate = dateObj[dateObj.length - 1].date ?? "";
+  const formattedDate = formatDayAndDate(firstDate, language);
+  const datesOnlyFirst = formatDateOnly(firstDate);
+  const datesOnlyLast = formatDateOnly(lastdate);
+  const dateLabel = getDateLabel({
+    dateObj,
+    formattedDate,
+    datesOnlyFirst,
+    datesOnlyLast,
+    firstDate,
+    language,
+  });
+
+  return (
+    <div className=" mb-16 border p-2 border-black text-black inline-block italic md:hidden">
+      {dateLabel}
+    </div>
+  );
+}
+
+export default EventDateLabel;
--- app/routes/($lang).program._index.tsx
+++ app/routes/($lang).program._index.tsx
@@ -11,6 +11,7 @@ import { loadQuery } from "../../cms/loader.server";
 import { QueryResponseInitial } from "@sanity/react-loader";
 import { useQuery } from "../../cms/loader";
 import { loadQueryOptions } from "cms/loadQueryOptions.server";
+import EventDateLabel from "~/components/EventLabels/EventDateLabel";
 
 export async function loader({ params, request }: LoaderFunctionArgs) {
   const { options } = await loadQueryOptions(request.headers);
@@ -70,55 +71,73 @@ export default function Program() {
     query: string;
     queryParams: Record<string, string>;
   };
-
   const { data } = useQuery<typeof initial.data>(query, queryParams, {
     initial,
   });
+
   const { setColor } = useBackgroundColor();
   const { t } = useTranslation();
   const [gifUrl, setGifUrl] = useState(urlFor(data?.gif?.asset?._ref || ""));
+
   useEffect(() => {
     setColor("bg-strongblue");
   }, [setColor]);
   const params = useParams();
   return (
-    <div className="flex flex-col grow items-center text-white font-serif">
-      <h1 className="text-5xl font-bold mb-12">{data?.title}</h1>
-      <div className="flex flex-col items-center text-center font-normal gap-4 text-xl py-12 px-0">
+    <div className="flex flex-col grow items-center text-black font-serif">
+      <h1 className="text-5xl font-bold mb-12 hidden">{data?.title}</h1>
+      <div className="flex md:flex-row flex-wrap w-[90vw]">
+        <div className="w-full md:w-1/3 max-sm:hidden"></div>
+        <div className="w-3/4 md:w-1/3 vertical-align: middle; mx-auto">
+          {data?.links?.map((link, index) => (
+            <>
+              <Link
+                key={index}
+                to={
+                  link.slug?.current
+                    ? `${params.lang === "en" ? "/en/event/" : "/event/"}${
+                        link.slug.current
+                      }`
+                    : ""
+                }
+                onMouseEnter={() => {
+                  setGifUrl(
+                    urlFor(
+                      link.gif?.asset?._ref ?? link.image.asset?._ref ?? ""
+                    )
+                  );
+                }}
+                onMouseOut={() => {
+                  data?.gif && setGifUrl(urlFor(data.gif.asset?._ref ?? ""));
+                }}
+                className=" align-middle md:w-1/3 md:text-center "
+                aria-label={`${t(texts.labelText)} ${link.title}`}
+              >
+                <div className="md:hidden aspect-square w-full">
+                  <img
+                    className="inline-block object-cover w-full h-full"
+                    src={urlFor(link?.image?.asset?._ref ?? "")}
+                    alt={link?.image?.alt ?? ""}
+                    key={index}
+                  />
+                </div>
+                <p className="hover:underline text-2xl lg:text-4xl mt-4 mb-2">
+                  {link.title}
+                </p>
+                {link.dates.length > 0 && (
+                  <EventDateLabel dateObj={link.dates} />
+                )}
+              </Link>
+            </>
+          ))}
+        </div>
         {data?.gif && (
           <img
             src={gifUrl}
-            className="absolute w-2/3 right-[10vw] sm:w-1/3  lg:w-1/4 lg:right-[20vw] lg:bottom-5"
             alt={data.gif.alt}
+            className=" hidden md:block w-1/3 max-h-[500px] object-cover top-0 right-0"
           />
         )}
-
-        {data?.links?.map((link, index) => (
-          <Link
-            key={index}
-            to={
-              link.slug?.current
-                ? `${params.lang === "en" ? "/en/event/" : "/event/"}${
-                    link.slug.current
-                  }`
-                : ""
-            }
-            onMouseEnter={() => {
-              setGifUrl(
-                urlFor(link.gif?.asset?._ref ?? link.image.asset?._ref ?? "")
-              );
-            }}
-            onMouseOut={() => {
-              data?.gif && setGifUrl(urlFor(data.gif.asset?._ref ?? ""));
-            }}
-            className="z-10"
-            aria-label={`${t(texts.labelText)} ${link.title}`}
-          >
-            <p className="p-4 hover:underline text-2xl lg:text-4xl">
-              {link.title}
-            </p>
-          </Link>
-        ))}
       </div>
       <div className="mt-auto flex flex-col items-center text-lg lg:text-xl w-4/5 lg:w-2/3 z-10">
         <Newsletter />
