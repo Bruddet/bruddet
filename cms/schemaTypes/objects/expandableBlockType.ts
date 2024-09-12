7255b47909fe7f19d69e355c57111972abda0862
Erling Van De Weijer <44138705+erlinvan@users.noreply.github.com>
2024-09-12 15:25:10 +0200

    Remove required from some fields in Sanity. (#229)
    * Remove required from some fields in Sanity.
    
    Add some missing linting as well
    
    * Remove warning after when text surpasses limit in expandable block.
    
    * bytt navn fra Selskap til Medie.
    
    * Move image and slug higher up in web editor, since they are some of the mroe important elements on pages.
    
    * Remove warning after charachter limit on content rather than title for expandable block.

--- cms/schemaTypes/objects/expandableBlockType.ts
+++ cms/schemaTypes/objects/expandableBlockType.ts
@@ -1,29 +1,28 @@
-import {defineField, defineType} from 'sanity'
-import {BlockElementIcon} from '@sanity/icons'
+import { defineField, defineType } from "sanity";
+import { BlockElementIcon } from "@sanity/icons";
 
 export const expandableBlockType = defineType({
-  name: 'expandableBlock',
-  title: 'Ekspanderbar blokk',
-  type: 'document',
+  name: "expandableBlock",
+  title: "Ekspanderbar blokk",
+  type: "document",
   icon: BlockElementIcon,
   fields: [
     defineField({
-      name: 'title',
-      type: 'text',
-      title: 'Tittel',
+      name: "title",
+      type: "text",
+      title: "Tittel",
       validation: (rule) => [
         rule.required().min(2).error(`Tittel er påkrevd`),
         rule.required().max(100).warning(`Anbefaler kortere innhold.`),
       ],
     }),
     defineField({
-      name: 'content',
-      type: 'text',
-      title: 'Innhold',
+      name: "content",
+      type: "text",
+      title: "Innhold",
       validation: (rule) => [
         rule.required().min(2).error(`Innhold er påkrevd`),
-        rule.required().max(100).warning(`Anbefaler kortere innhold.`),
       ],
     }),
   ],
-})
+});
--- cms/schemaTypes/personType.ts
+++ cms/schemaTypes/personType.ts
@@ -1,46 +1,46 @@
-import {defineField, defineType} from 'sanity'
-import {UserIcon} from '@sanity/icons'
+import { defineField, defineType } from "sanity";
+import { UserIcon } from "@sanity/icons";
 
 export const personType = defineType({
-  name: 'person',
-  title: 'Person',
-  type: 'document',
+  name: "person",
+  title: "Person",
+  type: "document",
   icon: UserIcon,
   preview: {
     select: {
-      title: 'name',
-      media: 'image',
+      title: "name",
+      media: "image",
     },
   },
   fields: [
     defineField({
-      name: 'name',
-      title: 'Navn',
-      type: 'string',
-      validation: (rule) => rule.required().min(2).max(50).error(`Må ha navn på minst 2 bokstaver`),
+      name: "name",
+      title: "Navn",
+      type: "string",
+      validation: (rule) =>
+        rule.required().min(2).max(50).error(`Må ha navn på minst 2 bokstaver`),
     }),
 
     defineField({
-      name: 'language',
-      type: 'string',
+      name: "language",
+      type: "string",
       readOnly: true,
       hidden: true,
     }),
     defineField({
-      name: 'image',
-      title: 'Bilde',
-      type: 'customImage',
-      description: 'Legg til et bilde',
-      validation: (rule) => rule.required(),
+      name: "image",
+      title: "Bilde",
+      type: "customImage",
+      description: "Legg til et bilde",
       options: {
         hotspot: true,
       },
     }),
     defineField({
-      name: 'text',
-      title: 'Biografi',
-      description: 'Hold det kort',
-      type: 'string',
+      name: "text",
+      title: "Biografi",
+      description: "Hold det kort",
+      type: "string",
     }),
   ],
-})
+});
