c71a7e7f4b6a6b3aec1eab81faab8a50262519ee
Anniken Rønvåg Syvertsen <anniken.syvertsen@bekk.no>
2024-09-12 08:03:16 +0200

    made 'all fields' default instead of 'innhold'

--- cms/schemaTypes/articleType.ts
+++ cms/schemaTypes/articleType.ts
@@ -1,66 +1,67 @@
-import {defineField, defineType} from 'sanity'
-import {isUniqueOtherThanLanguage} from '../structure/documentInternationalization'
-import {documentInternationalization} from '@sanity/document-internationalization'
+import { defineField, defineType } from "sanity";
+import { isUniqueOtherThanLanguage } from "../structure/documentInternationalization";
+import { documentInternationalization } from "@sanity/document-internationalization";
 
 export const articleType = defineType({
-  name: 'article',
-  title: 'Artikkel',
-  type: 'document',
+  name: "article",
+  title: "Artikkel",
+  type: "document",
   groups: [
-    {title: 'Innhold', name: 'content', default: true},
-    {title: 'Visuelt', name: 'visual'},
-    {title: 'SEO', name: 'seo'},
+    { title: "Innhold", name: "content" },
+    { title: "Visuelt", name: "visual" },
+    { title: "SEO", name: "seo" },
   ],
   preview: {
     select: {
-      title: 'title',
-      media: 'image',
+      title: "title",
+      media: "image",
     },
   },
   fields: [
     defineField({
-      name: 'title',
-      title: 'Tittel',
-      type: 'string',
-      group: 'content',
+      name: "title",
+      title: "Tittel",
+      type: "string",
+      group: "content",
       validation: (rule) =>
         rule
           .required()
           .min(2)
           .max(100)
           .error(
-            `Tittel er påkrevd for å poste en artikkel, minimum lengde på 2 og maksimum lengde på 100 tegn`,
+            `Tittel er påkrevd for å poste en artikkel, minimum lengde på 2 og maksimum lengde på 100 tegn`
           ),
     }),
     defineField({
-      name: 'language',
-      type: 'string',
+      name: "language",
+      type: "string",
       readOnly: true,
       hidden: true,
     }),
     defineField({
-      name: 'colorCombinationsDay',
-      title: 'Fargekombinasjon',
-      type: 'colorCombinationsDay',
-      group: 'visual',
-      validation: (rule) => [rule.required().error('Må velge farger')],
+      name: "colorCombinationsDay",
+      title: "Fargekombinasjon",
+      type: "colorCombinationsDay",
+      group: "visual",
+      validation: (rule) => [rule.required().error("Må velge farger")],
     }),
     defineField({
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {source: 'title', isUnique: isUniqueOtherThanLanguage},
-      hidden: ({document}) => !document?.title,
-      description: 'Url: bruddet.no/xxx',
-      group: 'seo',
-      validation: (rule) => [rule.required().error('Må ha en slug')],
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: { source: "title", isUnique: isUniqueOtherThanLanguage },
+      hidden: ({ document }) => !document?.title,
+      description: "Url: bruddet.no/xxx",
+      group: "seo",
+      validation: (rule) => [rule.required().error("Må ha en slug")],
     }),
     defineField({
-      name: 'text',
-      title: 'Tekst',
-      type: 'content',
-      description: 'Innhold: Mulighet for å legge inn tekst, bilde, video, sitat og anmeldelse',
-      group: 'content',
+      name: "text",
+      title: "Tekst",
+      type: "content",
+      description:
+        "Innhold: Mulighet for å legge inn tekst, bilde, video, sitat og anmeldelse",
+      group: "content",
       options: {
         documentInternationalization: {
           exclude: true,
@@ -68,53 +69,53 @@ export const articleType = defineType({
       },
     }),
     defineField({
-      name: 'image',
-      title: 'Hovedbilde',
-      type: 'customImage',
-      description: 'Bildet som plasseres øverst på siden',
-      group: 'visual',
+      name: "image",
+      title: "Hovedbilde",
+      type: "customImage",
+      description: "Bildet som plasseres øverst på siden",
+      group: "visual",
       options: {
         hotspot: true,
       },
     }),
     defineField({
-      name: 'video',
-      title: 'Video',
-      type: 'video',
-      description: 'Legg til en video',
-      group: 'visual',
+      name: "video",
+      title: "Video",
+      type: "video",
+      description: "Legg til en video",
+      group: "visual",
     }),
     defineField({
-      name: 'event',
-      type: 'reference',
-      description: 'Referer til valgt forestilling',
-      to: [{type: 'event'}],
+      name: "event",
+      type: "reference",
+      description: "Referer til valgt forestilling",
+      to: [{ type: "event" }],
       options: {
-        filter: ({document}) => {
+        filter: ({ document }) => {
           return {
-            filter: 'language == $lang',
-            params: {lang: document.language},
-          }
+            filter: "language == $lang",
+            params: { lang: document.language },
+          };
         },
         documentInternationalization: {
           exclude: true,
         },
       },
-      group: 'content',
+      group: "content",
     }),
     defineField({
-      name: 'metaTitle',
-      title: 'SEO tittel',
-      type: 'metaTitle',
-      group: 'seo',
-      validation: (rule) => [rule.required().error('Må ha SEO tittel')],
+      name: "metaTitle",
+      title: "SEO tittel",
+      type: "metaTitle",
+      group: "seo",
+      validation: (rule) => [rule.required().error("Må ha SEO tittel")],
     }),
     defineField({
-      name: 'metaDescription',
-      title: 'SEO beskrivelse',
-      type: 'metaDescription',
-      group: 'seo',
-      validation: (rule) => [rule.required().error('Må ha SEO beskrivelse')],
+      name: "metaDescription",
+      title: "SEO beskrivelse",
+      type: "metaDescription",
+      group: "seo",
+      validation: (rule) => [rule.required().error("Må ha SEO beskrivelse")],
     }),
   ],
-})
+});
