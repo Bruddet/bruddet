f5d699d4fb0f7d5a88dd93193a538da7d6c2593c
Erling Van De Weijer <44138705+erlinvan@users.noreply.github.com>
2024-09-20 09:04:17 +0200

    Eventpage image (#252)
    * Remove cropping of main image on event page, and make the image take up as much space as possible.
    
    * Make changes to main event image
    
    Remove cropping, make the image larger, and use svg image on top of the
    background image.
    
    * Set max height for event page image.

--- app/components/Masks/ImageEventPage.tsx
+++ app/components/Masks/ImageEventPage.tsx
@@ -1,69 +1,25 @@
-import SmallImageMask from "../../assets/smallImageMask";
-import BigImageMask from "../../assets/BigImageMask";
-import { getImageMask } from "../../utils/imageMask";
-
-interface MaskProps {
+interface ImageProps {
   url: string;
   alt: string;
-  scale: number;
-  imageMaskType: string;
+  svgUrl?: string;
+  svgAlt?: string;
 }
 
 export default function ImageEventPage({
   url,
   alt,
-  scale = 1,
-  imageMaskType,
-}: MaskProps) {
-  const maskType = getImageMask(imageMaskType);
-
-  if (maskType === "smallImageMask") {
-    return (
-      <>
-        <div className="flex flex-col  justify-center pt-3  items-center w-full">
-          <SmallImageMask
-            scaleProp={scale || 1}
-            widthSvg={253}
-            heightSvg={299}
-          />
-          <img
-            src={url}
-            alt={alt}
-            style={{
-              clipPath: "url(#smallImageMask)",
-              objectFit: "cover",
-              width: `${253 * scale}px`,
-              height: `${299 * scale}px`,
-            }}
-          />
-        </div>
-      </>
-    );
-  } else if (maskType === "bigImageMask") {
-    return (
-      <>
-        <div className="flex flex-col  justify-center pt-0  items-center w-full">
-          <BigImageMask scaleProp={scale || 1} widthSvg={389} heightSvg={427} />
-          <img
-            src={url}
-            alt={alt}
-            style={{
-              clipPath: "url(#bigImageMask)",
-              objectFit: "cover",
-              width: `${389 * scale}px`,
-              height: `${427 * scale}px`,
-            }}
-          />
-        </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div className="flex flex-col  justify-center pt-3  items-center w-full">
-          <img src={url} alt={alt} />
-        </div>
-      </>
-    );
-  }
+  svgUrl,
+  svgAlt,
+}: ImageProps) {
+  return (
+    <div
+      className="flex justify-center aspect-[9/16] sm:aspect-[16/9] grow bg-cover max-h-[60vh] sm:max-h-[80vh] bg-center w-full  bg-no-repeat max-w-[2000px] mx-auto font-serif"
+      style={{
+        backgroundImage: `url(${url})`,
+      }}
+      aria-label={alt}
+    >
+      <img className="md:w-1/2 max-w-[80%]" src={svgUrl} alt={svgAlt} />
+    </div>
+  );
 }
