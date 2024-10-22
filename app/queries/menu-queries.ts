c021aeb25b6f5afdbbc35f5b0286c910483a5c26
Jakob Helgøy <jakobnhelgoy@gmail.com>
2024-10-22 20:55:22 +0200

    fix menu-query

--- app/queries/menu-queries.ts
+++ app/queries/menu-queries.ts
@@ -3,6 +3,18 @@ import groq from "groq";
 
 export function getMenuPageQuery(params: Params<string>) {
   const lang = params.lang ?? "nb";
-  const MENUPAGE_QUERY = groq`*[_type=="menupage" && language==$lang]{title, metaTitle, metaDescription, links[]->{_type, title, slug}}[0]`;
+  const MENUPAGE_QUERY = groq`*[_type == "menupage" && language == $lang] {
+    title,
+    metaTitle, 
+    metaDescription, 
+    links[]->{title,
+    slug,
+    _type,
+    text[style in ["h1","h2", "h3", "h4", "h5"]]{
+    defined(_key) => {_key},
+    "subtitle": children[0].text,
+    "slug": ^.slug.current
+    }[defined(subtitle)],
+    }}[0]`;
   return { query: MENUPAGE_QUERY, params: { lang } };
 }
